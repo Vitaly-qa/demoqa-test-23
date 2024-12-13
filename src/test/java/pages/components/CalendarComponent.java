b94ab784b8fc83410bd9575c36fa9515ee22e8e7
Vitaly Kuzmin <vitalyqa34@gmail.com>
2024-12-13 14:39:40 +0300

    Убрали харкод

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -8,10 +8,9 @@ public class CalendarComponent {
 
     public void setDate(String day, String month, String year){
         $(".react-datepicker__month-select").click();
-        $(".react-datepicker__month-select").selectOption("July");
-        $(".react-datepicker__year-select").click();
-        $(".react-datepicker__year-select").selectOption("1990");
-        $(".react-datepicker__day.react-datepicker__day--008").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0"+day+":not(.react-datepicker__day--outside-month)").click();
 
 
     }
