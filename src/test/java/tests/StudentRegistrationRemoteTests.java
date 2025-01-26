35ba9fab6a094bab188f395fd4ad8383e6eefaa9
Vitaly Kuzmin <vitalyqa34@gmail.com>
2025-01-26 21:34:59 +0300

    add property test

--- src/test/java/tests/StudentRegistrationRemoteTests.java
+++ src/test/java/tests/StudentRegistrationRemoteTests.java
@@ -33,7 +33,7 @@ public class StudentRegistrationRemoteTests {
 //        Configuration.browser = "chrome";
         Configuration.timeout = 10000;
 //        Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
